1311e4f6e2a53c5d7efdf9f09f4f6265fc9c4e61
Kento75 <kento2github@gmail.com>
2019-01-21 22:05:48 +0900

    Add base app

---
+++ src/main/kotlin/net/kento75/kotlinAdmin/KotlinAdminApplication.kt
@@ -0,0 +1,12 @@
+package net.kento75.kotlinAdmin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinAdminApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinAdminApplication>(*args)
+}
+
---
+++ src/test/kotlin/net/kento75/kotlinAdmin/KotlinAdminApplicationTests.kt
@@ -0,0 +1,17 @@
+package net.kento75.kotlinAdmin
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinAdminApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
